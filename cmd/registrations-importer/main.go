876ff84f2460998485747be97dd28379a47a4eaf
Alexander Nielsen <hitecherik@gmail.com>
2020-06-19 23:13:01 +0100

    Add basic tabbycat integration

--- cmd/registrations-importer/main.go
+++ cmd/registrations-importer/main.go
@@ -4,18 +4,21 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/hitecherik/Imperial-Online-IV/pkg/tabbycat"
 	"github.com/hitecherik/Imperial-Online-IV/pkg/zoom"
 	"github.com/joho/godotenv"
 )
 
-type credentials struct {
+type options struct {
 	zoomApiKey     string
 	zoomApiSecret  string
+	zoomMeetingId  string
 	tabbycatApiKey string
+	tabbycatUrl    string
+	tabbycatSlug   string
 }
 
-var creds credentials
-var meetingId string
+var opts options
 
 func bail(err error) {
 	if err != nil {
@@ -26,17 +29,31 @@ func bail(err error) {
 func init() {
 	bail(godotenv.Load())
 
-	creds.zoomApiKey = os.Getenv("ZOOM_API_KEY")
-	creds.zoomApiSecret = os.Getenv("ZOOM_API_SECRET")
-	creds.tabbycatApiKey = os.Getenv("TABBYCAT_API_KEY")
-	meetingId = os.Getenv("ZOOM_MEETING_ID")
+	opts.zoomApiKey = os.Getenv("ZOOM_API_KEY")
+	opts.zoomApiSecret = os.Getenv("ZOOM_API_SECRET")
+	opts.zoomMeetingId = os.Getenv("ZOOM_MEETING_ID")
+
+	opts.tabbycatApiKey = os.Getenv("TABBYCAT_API_KEY")
+	opts.tabbycatUrl = os.Getenv("TABBYCAT_URL")
+	opts.tabbycatSlug = os.Getenv("TABBYCAT_SLUG")
 }
 
 func main() {
-	zoom := zoom.New(creds.zoomApiKey, creds.zoomApiSecret)
+	zoom := zoom.New(opts.zoomApiKey, opts.zoomApiSecret)
 
-	registrants, err := zoom.GetRegistrants(meetingId)
+	registrants, err := zoom.GetRegistrants(opts.zoomMeetingId)
 	bail(err)
 
-	fmt.Printf("Registrants:\n%+v\n", registrants)
+	fmt.Printf("Registrants:\n%+v\n\n", registrants)
+
+	tabbycat := tabbycat.New(opts.tabbycatApiKey, opts.tabbycatUrl, opts.tabbycatSlug)
+	teams, err := tabbycat.GetTeams()
+	bail(err)
+
+	fmt.Printf("Teams:\n%+v\n\n", teams)
+
+	adjudicators, err := tabbycat.GetAdjudicators()
+	bail(err)
+
+	fmt.Printf("Adjudicators:\n%+v\n", adjudicators)
 }
---
+++ pkg/tabbycat/tabbycat.go
@@ -0,0 +1,90 @@
+package tabbycat
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Tabbycat struct {
+	apiKey   string
+	client   *http.Client
+	endpoint string
+}
+
+type Team struct {
+	Id       int64         `json:"id"`
+	Speakers []Participant `json:"speakers"`
+}
+
+type Participant struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
+func New(apiKey string, url string, slug string) *Tabbycat {
+	return &Tabbycat{
+		apiKey:   apiKey,
+		client:   &http.Client{},
+		endpoint: fmt.Sprintf("%v/api/v1/tournaments/%v/", url, slug),
+	}
+}
+
+func (t *Tabbycat) GetAdjudicators() ([]Participant, error) {
+	req, err := http.NewRequest(http.MethodGet, t.endpoint+"adjudicators", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	t.authorize(req)
+	resp, err := t.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var adjudicators []Participant
+	if err := json.Unmarshal(body, &adjudicators); err != nil {
+		return nil, err
+	}
+
+	return adjudicators, nil
+}
+
+func (t *Tabbycat) GetTeams() ([]Team, error) {
+	req, err := http.NewRequest(http.MethodGet, t.endpoint+"teams", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	t.authorize(req)
+	resp, err := t.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var teams []Team
+	if err := json.Unmarshal(body, &teams); err != nil {
+		return nil, err
+	}
+
+	return teams, nil
+}
+
+func (t *Tabbycat) authorize(req *http.Request) {
+	req.Header.Add("Authorization", fmt.Sprintf("Token %v", t.apiKey))
+}
